a8cc7d70bb4b5d1c66044a177cabfe082ad6491c
Viktor Khalezov <viktor-khalezov@mail.ru>
2022-06-18 19:08:24 +0300

    Homework lesson №6

--- model-module/src/main/java/ru/gb/modelmodule/service/ProductService.java
+++ model-module/src/main/java/ru/gb/modelmodule/service/ProductService.java
@@ -10,6 +10,7 @@ import ru.gb.modelmodule.dto.ProductDto;
 import ru.gb.modelmodule.entity.Product;
 import ru.gb.modelmodule.facade.InterfaceFacade;
 import ru.gb.modelmodule.mapper.ProductMapper;
+import ru.gb.modelmodule.unitOfWork.UnitOfWork;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,19 +20,38 @@ import java.util.stream.Collectors;
 public class ProductService {
 
 
-    private final InterfaceFacade interfaceFacade;
+    private final UnitOfWork unitOfWork;
+//    private final InterfaceFacade interfaceFacade;
 
     public List<ProductDto> getProducts() {
-        return interfaceFacade.findAll();
+        return unitOfWork.readAll();
     }
 
-
-    public ProductDto save(ProductDto productDto) {
-        return interfaceFacade.save(productDto);
+    public ProductDto getProductById(Long id) {
+        return unitOfWork.readOneProduct(id);
     }
 
-    public void delete(Long id) {
-        interfaceFacade.deleteById(id);
+    public void insert(ProductDto productDto) {
+        unitOfWork.registerNew(productDto);
+        unitOfWork.commit();
     }
+
+    public void update(ProductDto productDto) {
+        unitOfWork.registerUpdate(productDto);
+        unitOfWork.commit();
+    }
+
+    public void delete(ProductDto productDto) {
+        unitOfWork.registerDelete(productDto);
+        unitOfWork.commit();
+    }
+
+//    public ProductDto save(ProductDto productDto) {
+//        return interfaceFacade.save(productDto);
+//    }
+//
+//    public void delete(Long id) {
+//        interfaceFacade.deleteById(id);
+//    }
 
 }
